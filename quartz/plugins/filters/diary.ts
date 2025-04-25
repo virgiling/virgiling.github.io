fbd0c800190c1207c2f1f7843e21ff39dc2051fd
virgil <virgiling7@gmail.com>
2025-04-25 13:43:58 +0800

    chore: add my diary into repo

--- quartz/plugins/filters/diary.ts
+++ quartz/plugins/filters/diary.ts
@@ -3,7 +3,7 @@ import { QuartzFilterPlugin } from "../types"
 export const RemoveDiary: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveDiary",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const diaryFlag: boolean = false || vfile.path?.includes("Daily") || vfile.path?.includes("daily")
+    const diaryFlag: boolean = vfile.data.frontmatter?.tags?.includes("日记") || false
     return !diaryFlag
   },
 })
