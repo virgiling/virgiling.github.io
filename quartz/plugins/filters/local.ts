6379eb8b18377015abdc65048120b17b4abf15bf
Dian Ling <virgiling7@gmail.com>
2025-11-12 21:33:04 +0800

    using submodule (#69)
    * rm content
    
    * using submodules

--- quartz/plugins/filters/local.ts
+++ quartz/plugins/filters/local.ts
@@ -1,9 +1,12 @@
 import { QuartzFilterPlugin } from "../types"
 
 export const RemoveLocal: QuartzFilterPlugin<{}> = () => ({
-    name: "RemoveDiary",
+    name: "RemoveLocal",
     shouldPublish(_ctx, [_tree, vfile]) {
         const localFlag: boolean = vfile.data.frontmatter?.tags?.includes("Local") || false
-        return !localFlag
+        const draw: boolean = vfile.data.filePath?.includes("Excalidraw") || false
+        const private_file: boolean = vfile.data.filePath?.includes("private") || false
+        const task: boolean = vfile.data.filePath?.includes("task") || false
+        return !localFlag && !draw && !private_file && !task
     },
 })
