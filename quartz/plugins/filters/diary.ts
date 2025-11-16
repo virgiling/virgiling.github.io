407c52689abf0106bc9ddebad7e58203c125daa0
Dian Ling <virgiling7@gmail.com>
2025-11-16 11:58:38 +0800

    fix: hide daily and project

--- quartz/plugins/filters/diary.ts
+++ quartz/plugins/filters/diary.ts
@@ -3,7 +3,8 @@ import { QuartzFilterPlugin } from "../types"
 export const RemoveDiary: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveDiary",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const diaryFlag: boolean = vfile.data.frontmatter?.tags?.includes("日记") || false
-    return !diaryFlag
+    const diaryFlag: boolean = vfile.data.filePath?.includes("10-daily") || false
+    const projectFlag: boolean = vfile.data.filePath?.includes("05-project") || false
+    return !diaryFlag && !projectFlag
   },
 })
